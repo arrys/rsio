b26e5533b7559c9f92d0e92cbf773cd7c2878252
Arkadiusz Ryś <Arkadiusz.Michal.Rys@gmail.com>
2025-04-20 22:53:46 +0200

    Fix cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -28,12 +28,12 @@ def test_cli_package(caplog):
         assert result.exit_code == 0
         assert "Checking" in caplog.text
 
-
-    package_name = command
-    with TemporaryPath(package_name):
-        result = runner.invoke(cli, [command, "--verbose", "--create", "--name", package_name])
-        assert result.exit_code == 0
-        assert "package created" in result.output
+        package_name = command
+        with TemporaryPath(package_name):
+            result = runner.invoke(cli, [command, "--verbose", "--create", "--name", package_name])
+            assert result.exit_code == 0
+            # assert "package created" in result.output
+            assert "package created" in caplog.text
 
 def test_cli_transformation():
     """"""
