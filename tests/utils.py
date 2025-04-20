f196d8ee3ff542525c79103fdfab199501f3089e
Arkadiusz Ryś <Arkadiusz.Michal.Rys@gmail.com>
2025-04-20 22:52:32 +0200

    Remove unneedded resolve from test path util

--- tests/utils.py
+++ tests/utils.py
@@ -6,7 +6,7 @@ import shutil
 class TemporaryTemplatedPath:
     def __init__(self, template_file_path: Path | str | None, extraction_path: Path | str, remove_after_completion: bool = True):
         self.template_file_path = Path(template_file_path) if isinstance(template_file_path, str) else template_file_path
-        self.extraction_path = (extraction_path if isinstance(extraction_path, Path) else Path(extraction_path)).resolve()
+        self.extraction_path = extraction_path if isinstance(extraction_path, Path) else Path(extraction_path)
         self.remove_after_completion = remove_after_completion
 
     def __enter__(self):
