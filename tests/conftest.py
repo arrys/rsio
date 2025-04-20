3ddd5f06726d576b3c1c4c0b003c6523d60a3069
Arkadiusz Ryś <Arkadiusz.Michal.Rys@gmail.com>
2025-04-20 22:53:05 +0200

    Suppress all logs during testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,6 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def suppress_logs():
+    """Always suppress all logging output during tests."""
     logging.getLogger().handlers.clear()
     logging.getLogger().addHandler(logging.NullHandler())
