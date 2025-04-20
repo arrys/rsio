2653b8c93323de1b164b1ca9f69b03d73c49f7ae
Arkadiusz Ryś <Arkadiusz.Michal.Rys@gmail.com>
2025-04-20 22:54:11 +0200

    Disable broken launch test

--- tests/test_launch.py
+++ tests/test_launch.py
@@ -1,4 +1,8 @@
-# from rpio.pyLauncher.pyLauncher import launch
-#
-#
-# launch('input/launch.xml')
+from pathlib import Path
+from rpio.launcher.launcher import launch
+from tests.utils import TemporaryTemplatedPath
+
+
+# def test_launch():
+#     with TemporaryTemplatedPath(Path(__file__).resolve().parent / Path("data/hell0.zip"), Path.cwd() / "package", False) as package_path:
+#         launch(package_path / "launch.xml")
