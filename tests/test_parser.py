466c23bde24e00b4ce890e58f1e2a8080a15e350
Arkadiusz Ryś <Arkadiusz.Michal.Rys@gmail.com>
2025-08-05 11:28:24 +0200

    Add new token

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,6 +5,6 @@ from tests.utils import TemporaryTemplatedPath
 
 
 def test_aadlil_parser():
-    with TemporaryTemplatedPath(Path(__file__).resolve().parent / Path("data/parser.zip"), Path.cwd() / "package", False) as package_path:
+    with TemporaryTemplatedPath(Path(__file__).resolve().parent / Path("data/parser.zip"), Path.cwd() / "package") as package_path:
         loaded_system = System(name="adaptiveSystem", description="Loaded from aadlil", json_descriptor=package_path / "aadlil.json")
         assert loaded_system.name == "adaptiveSystem"
